e18613972017332b554bbe5a9dde90f4fdc74d07
selvakumar kasi <Selvakumar.Kasi@rabobank.nl>
2023-02-07 05:38:50 +0530

    added new port for the app

---
+++ src/test/java/cts/rabo/loanmanagementsystem/LoanManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package cts.rabo.loanmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
